132954b1095cf812754e0b146c1944abf790a699
Alejandro Alonso <aalonsopuig@gmail.com>
2023-11-19 23:21:03 +0100

    .

--- src/d_Multimedia_AbstractFactory/MainClass.java
+++ src/d_Multimedia_AbstractFactory/MainClass.java
@@ -5,18 +5,6 @@ package d_Multimedia_AbstractFactory;
  * Factory para crear diferentes tipos de reproductores multimedia. 
  * Los reproductores pueden ser de audio o de video, y cada tipo 
  * tendrá dos variantes: Básico y Avanzado.
- * 
- * Requisitos:
- * 1. Crear una interfaz MediaPlayer que defina los métodos play() y pause().
- * 2. Implementar dos tipos de reproductores: AudioPlayer y VideoPlayer, 
- *    cada uno con dos variantes: Básico y Avanzado.
- * 3. Desarrollar una interfaz abstracta MediaFactory con métodos para crear 
- *    reproductores de audio y video. 
- * 4. Implementar dos fábricas concretas: BasicMediaFactory y AdvancedMediaFactory, 
- *    que creen las variantes Básica y Avanzada de los reproductores de audio y 
- *    video, respectivamente.
- * 5. Escribir una clase de prueba que demuestre el uso de las fábricas para 
- *    crear diferentes tipos de reproductores.
  */
 
 
